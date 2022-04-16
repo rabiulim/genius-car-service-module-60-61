4830d0c366cfd6dd2d3fc9480735ecaac0fcb11b
rabiulim <rabiulim2022@gmail.com>
2022-04-16 12:19:38 +0600

    genius done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -41,6 +41,7 @@ const Header = () => {
                                     <Nav.Link as={Link} to="/login">
                                         Login
                                     </Nav.Link>}
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
